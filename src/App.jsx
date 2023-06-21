1da3051a7ae7d587218bfb46d99192fe4a8e5e51
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-06-21 18:44:09 -0300

    comienzo de proyecto de porfolio, trabajando en el fondo estrellado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import FallingStars from "./components/falling-stars";
+
+function App() {
+    return (
+      <main>
+        <FallingStars>
+          <h1>HOLA AMIGOS DE YOUTUBE</h1>
+        </FallingStars>
+      </main>
+    )
+  
+}
+
+export default App
