f4862a68644a841cf7115dbd479f09ac7888daa2
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-06-28 18:24:02 -0300

    comienzo de estilos de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Header = () => {
     return (
         <div className="flex justify-around h-16 bg-neutral-950">
-            <h2 className="text-indigo-900 text-6xl">HOLA</h2>
+            <div className="text-indigo-900 text-6xl">
+            <FontAwesomeIcon icon="fa-solid fa-quote-left fa-lg" />            
+            </div>
 
             <nav className="flex items-center justify-around list-none gap-1 w-2/5 bg-red-600">
                 <li className="text-gray-50"><a href="">About Me</a></li>
