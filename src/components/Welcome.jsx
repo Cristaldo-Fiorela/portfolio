6c2ca63e72c30e6189f04f695bf679bbe0a3d473
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-04 17:58:31 -0300

    probando posicionamiento y estilos de welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,18 +5,18 @@ return (
     <>
         <div className='h-screen flex flex-col justify-center items-center text-gray-100'>
             <div className="flex gap-5">
-                <p className="self-start text-5xl">Hi I'm</p>
+                <p className="self-start text-5xl text-gray-300">Hi I'm</p>
                 <h1 className="welcome-name flex flex-col justify-center text-9xl">
-                    <span>Fiorela&nbsp;</span> 
-                    <span>Cristaldo</span> 
-                </h1>
+                            <span>Fiorela&nbsp;</span> 
+                            <span>Cristaldo</span> 
+                        </h1>
             </div>
             <div className="flex flex-col">
-                <h2 className="text-4xl tracking-widest">FrontEnd Developer Junior</h2>
-                <button className="uppercase ">Contactame</button>
-            </div>
-        </div>
 
+                    <h2 className="text-4xl trabajo">FrontEnd Developer Junior</h2>
+                </div>
+            {/* <button className="uppercase ">Contactame</button> */}
+        </div>
     </>
 )
 }
