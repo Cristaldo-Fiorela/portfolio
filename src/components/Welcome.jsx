dbedd6f7b2a06970b6af4725c1167b4fb3b5484a
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-06 17:59:13 -0300

    creacion de boton de contacto en welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,18 +3,31 @@
 const Welcome = () => {
 return (
     <>
-        <div className='h-screen flex flex-col justify-center items-center text-gray-100'>
+        <div className='h-screen flex flex-col justify-center items-center text-gray-200 gap-6'>
             <div className="flex gap-5">
-                <p className="self-start text-5xl text-gray-300">Hi I'm</p>
-                <h1 className="welcome-name flex flex-col justify-center text-9xl">
+                <p className="self-start text-5xl text-gray-400">Hi I'm</p>
+                {/* <h1 className="welcome-name flex flex-col justify-center text-9xl">
                             <span>Fiorela&nbsp;</span> 
                             <span>Cristaldo</span> 
-                        </h1>
+                        </h1> */}
+                        <div className="flex flex-col">
+                            <h1 className="welcome-name flex flex-col justify-center text-9xl">
+                                <span>Fiorela&nbsp;</span> 
+                                <span>Cristaldo</span> 
+                            </h1>
+                            <h2 className="text-4xl underline-offset-8 font-extrabold tracking-wider">
+                                FrontEnd Developer Junior
+                            </h2>
+                        </div>
             </div>
-            <div className="flex flex-col">
-
-                    <h2 className="text-4xl trabajo">FrontEnd Developer Junior</h2>
-                </div>
+            <div>
+                <button><a className="uppercas px-4 py-2 rounded-md text-black border-double font-bold" href="#contact-section">Contact Me</a></button>
+            </div>
+            {/* <div className="flex flex-col">
+                    <h2 className="text-4xl px-5 rounded-lg trabajo uppercase font-extrabold">
+                        FrontEnd Developer Junior uwu
+                    </h2>
+            </div> */}
             {/* <button className="uppercase ">Contactame</button> */}
         </div>
     </>
