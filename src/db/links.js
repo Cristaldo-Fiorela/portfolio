e45d70986801b3000450ccb370c70544ab46075f
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-02-19 16:23:44 -0300

    portfolio 2026

--- src/db/links.js
+++
@@ -1,9 +0,0 @@
-const cvSpanish = 'https://drive.google.com/file/d/1c6gwb21RlpnsBcyR3tWmsWw35ITt7RGp/view?usp=sharing';
-const logo = 'https://i.imgur.com/yasyMtb.png';
-const logoWhite = 'https://i.imgur.com/Ik3YTTD.png';
-
-export {
-  cvSpanish,
-  logo,
-  logoWhite,
-}
