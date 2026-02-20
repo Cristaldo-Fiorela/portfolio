3f788cdf2b795cab3fb1726f8c0b3d6969fca1c3
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-02-20 14:48:22 -0300

    correccion de menu de links de header. se implementa menu para mobile

---
+++ src/components/FullScreenMenu.jsx
@@ -0,0 +1,32 @@
+import { Icon } from "@iconify/react"
+import { cvSpanish } from "../db/links"
+
+const FullScreenMenu = ( { setMenuOpen }) => {
+  return (
+      <div className="fixed inset-0 bg-neutral-900 z-50 flex flex-col items-center justify-center md:hidden">
+        
+        <button 
+          className="absolute top-6 right-6 text-neutral-100 cursor-pointer"
+          onClick={() => setMenuOpen(false)}
+        >
+          <Icon icon="mdi:close" width="3rem" height="3rem" id='close' />
+        </button>
+
+        {/* Links */}
+        <nav className="flex flex-col items-center gap-10 text-2xl font-semibold text-neutral-100">
+          <a href="#about"   onClick={() => setMenuOpen(false)}>Sobre Mi</a>
+          <a href="#skills"  onClick={() => setMenuOpen(false)}>Habilidades</a>
+          <a href="#projects" onClick={() => setMenuOpen(false)}>Proyectos</a>
+          <a 
+            href={cvSpanish} target='_blank' rel='noreferrer' 
+            onClick={() => setMenuOpen(false)}
+            className="btn-cv bg-linear-to-bl from-purple via-pink to-yellow px-8 py-3 rounded-4xl text-neutral-800 font-extrabold hover:opacity-80 border-2 border-yellow transition hover:scale-105 text-xl mt-4"
+          >
+            CV
+          </a>
+        </nav>
+      </div>
+  )
+}
+
+export default FullScreenMenu
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,34 @@
-import { cvSpanish, logo, logoWhite } from '../db/links.js';
+import { useState } from 'react';
+import { cvSpanish, logoWhite } from '../db/links.js';
+import FullScreenMenu from './FullScreenMenu.jsx';
 
 const Header = ({  navbarRef, whiteLogo }) => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header ref={navbarRef} className="bg-neutral-800 text-neutral-100 my-3 border-2 border-neutral-700 p-2 rounded-4xl grid grid-cols-5">
-      <img src={logoWhite} alt="hola"  className='h-12 ml-5 col-span-1'/>
-      <nav className='col-span-4 flex justify-around items-center [&_a:not(.btn-cv)]:hover:text-pink'>
+    <header ref={navbarRef} className="bg-neutral-800 text-neutral-100 my-3 border-2 border-neutral-700 p-2 rounded-4xl flex justify-between md:grid md:grid-cols-5">
+      <img src={logoWhite} alt="hola"  className='h-8 md:h-12 ml-5 col-span-1'/>
+
+      <button 
+        className="md:hidden cursor-pointer z-50 mr-5"
+        onClick={() => setMenuOpen(true)}
+      >
+        <span className="block w-6 h-0.5 bg-neutral-100 mb-1.5"/>
+        <span className="block w-6 h-0.5 bg-neutral-100 mb-1.5"/>
+        <span className="block w-6 h-0.5 bg-neutral-100"/>
+      </button>
+
+      <nav className='hidden md:flex col-span-4 justify-around items-center [&_a:not(.btn-cv)]:hover:text-pink'>
         <a href="#about">Sobre Mi</a>
         <a href="#skills">Habilidades</a>
         <a href="#projects">Proyectos</a>
         <a href={cvSpanish} target='_blank' rel='noreferrer' className="btn-cv bg-linear-to-bl from-purple via-pink to-yellow px-6 py-2 rounded-4xl text-neutral-800 font-extrabold hover:opacity-80 border-2 border-yellow transition hover:scale-105">CV</a>      
       </nav>
+
+      {menuOpen && (
+        <FullScreenMenu setMenuOpen={setMenuOpen} />
+      )}
     </header>
   )
 }
