cb0c01a2e94c54a51641cc57dd3658c0d7e3afb8
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-02-24 16:18:25 -0300

    seccion de skills terminada

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react';
 
 const Skill = ({ name, icon }) => {
   return (
-    <p className='bg-neutral-100 text-black w-fit flex gap-1.5 p-1 px-2 rounded-xl font-bold' >
+    <p className='bg-neutral-100 text-black w-fit flex gap-1.5 p-1 px-2 rounded-xl font-bold hover:scale-105' >
       {name}
       <Icon icon={icon} width="1.7rem" height="1.7rem" /> 
     </p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,15 +5,16 @@ import Skill from './Skill';
 const Skills = () => {
 
   return (
-    <section id="skills">
-      <h2 className="section-title">
-        Habilidades {""}
-      <Icon icon="akar-icons:sparkles" width="3rem" height="3rem" />
-      </h2>
-      <div className="skill-container">
-        <section className='bg-pink/20 text-pink rounded-2xl flex flex-col p-5 border-2 gap-2'>
-          <h3 className="font-family-heading text-2xl">Desarrollo Frontend</h3>
-          <div className="flex flex-wrap gap-2">
+    <section id="skills" className='my-10 md:w-4/5 self-center flex flex-col gap-3'>
+      <div className='font-family-heading flex gap-2 text-2xl items-center justify-center'>
+        <h2 className="">Habilidades</h2>
+        <Icon icon="akar-icons:sparkles" width="3rem" height="3rem" />
+      </div>
+     
+      <div className="flex flex-col gap-5 md:grid md:grid-cols-2">
+        <section className='bg-pink/20 text-pink rounded-2xl flex flex-col p-5 border-2 gap-2 md:col-span-1'>
+          <h3 className="font-family-heading text-2xl">Frontend</h3>
+          <div className="flex flex-wrap gap-2 md:">
             {SKILLS?.front?.map(({id, name, icon}) => (
                 <Skill 
                 key={id}
@@ -24,33 +25,31 @@ const Skills = () => {
           </div>
         </section>
 
-        <div className="skill-container-small">
-          <section className='bg-yellow/20 text-yellow rounded-2xl flex flex-col p-5 border-2 gap-2'>
-            <h3 className="font-family-heading text-2xl">Desarrollo Backend</h3>
-            <div className='flex flex-wrap gap-2'>
-              {SKILLS?.back?.map(({id, name, icon}) => (
-                  <Skill 
-                  key={id}
-                  name={name}
-                  icon={icon}
-                />
-              ))}
-            </div>
-          </section>
+        <section className='bg-yellow/20 text-yellow rounded-2xl flex flex-col p-5 border-2 gap-2 md:col-span-1'>
+          <h3 className="font-family-heading text-2xl">Backend</h3>
+          <div className='flex flex-wrap gap-2'>
+            {SKILLS?.back?.map(({id, name, icon}) => (
+                <Skill 
+                key={id}
+                name={name}
+                icon={icon}
+              />
+            ))}
+          </div>
+        </section>
 
-          <section className='bg-purple/20 text-purple rounded-2xl flex flex-col p-5 border-2 gap-2'>
-            <h3 className="font-family-heading text-2xl">Dev Tools</h3>
-            <div className='flex flex-wrap gap-2'>
-              {SKILLS?.tools?.map(({id, name, icon}) => (
-                  <Skill 
-                  key={id}
-                  name={name}
-                  icon={icon}
-                />
-              ))}
-            </div>
-          </section>
-        </div>
+        <section className='bg-purple/20 text-purple rounded-2xl flex flex-col p-5 border-2 gap-2 md:col-span-2'>
+          <h3 className="font-family-heading text-2xl">Herramientas</h3>
+          <div className='flex flex-wrap gap-2'>
+            {SKILLS?.tools?.map(({id, name, icon}) => (
+                <Skill 
+                key={id}
+                name={name}
+                icon={icon}
+              />
+            ))}
+          </div>
+        </section>
 
       </div>
     </section>
