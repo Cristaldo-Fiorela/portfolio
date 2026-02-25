ba65a1377f7e46392a58fb60c572b5e7c6e14ed9
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-02-24 23:17:28 -0300

    scale para pill

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -5,7 +5,7 @@ const Pill = ({
   className = '',
 }) => {
   return (
-    <div className={`${bgColor} ${textColor} border-2 rounded-4xl w-fit py-1 px-3 ${className}`}>
+    <div className={`${bgColor} ${textColor} border-2 rounded-4xl w-fit py-1 px-3 ${className} hover:scale-105`}>
       {children}
     </div>
   )
