f4fc4658c3e8b203bd79978877dc6ee84e981031
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-02-24 22:11:43 -0300

    estilos a contenedor principal de proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,12 @@ import ProjectCard from './ProjectCard';
 
 const Projects = () => {
   return (
-    <section id="projects">
-      <h2 className="section-title">
-        Proyectos {""}
+    <section id="projects" className='flex flex-col gap-5'>
+      <div className='font-family-heading flex gap-2 text-2xl items-center justify-center'>
+        <h2 className="">Proyectos</h2>
         <Icon icon="akar-icons:sparkles" width="3rem" height="3rem" />
-      </h2>
-      <div className='projects-container'>
+      </div>
+      <div className='flex flex-col gap-5'>
         {DB.map( project => (
           <ProjectCard 
             id={project.id}
@@ -22,25 +22,7 @@ const Projects = () => {
 
           />
         ))
-
         }
-        {/* { DB.map(project => (
-          <div 
-            className="project-card"
-            key={project.id}
-            style={{backgroundImage: `url('${project.image}')`}}
-          >
-            <div className='card-body'>
-              <div className="tech-tags">
-                {project.tech.map( (tech, i) => (
-                  <p key={i}>#{tech}</p>
-                ))}
-              </div>
-              <h4>{project.name}</h4>
-            </div>
-          </div>
-          ))
-        } */}
       </div>
     </section>
   )
