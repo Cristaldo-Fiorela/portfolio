bd3855948ddca41d38accca6f0dfb16da36f9bf2
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-02-24 23:52:42 -0300

    cv-link-febrero-2026

--- src/db/links.js
+++ src/db/links.js
@@ -1,4 +1,4 @@
-const cvSpanish = 'https://drive.google.com/file/d/1c6gwb21RlpnsBcyR3tWmsWw35ITt7RGp/view?usp=sharing';
+const cvSpanish = 'https://drive.google.com/file/d/1_vusmtO5s6NX7KKLY0_319hlX0HurMVC/view?usp=sharing';
 const logo = 'https://i.imgur.com/yasyMtb.png';
 const logoWhite = 'https://i.imgur.com/Ik3YTTD.png';
 
