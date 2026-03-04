6bd4aa2e4cfb3c515cf0d074d24bf70ef69caf6f
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2026-03-04 15:16:52 -0300

    detalles estilos cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({
   certificationProvider 
 }) => {
   return (
-    <article className={`bg-neutral-800/70 p-4 rounded-2xl border-2 w-full ${borderColor} flex flex-col gap-3 justify-between`}>
+    <article className={`bg-neutral-800/70 p-4 rounded-2xl border-2 w-full ${borderColor} flex flex-col gap-3 justify-between hover:scale-102`}>
       <div className="flex justify-between">
         <div className={`bg-neutral-600/40 w-fit p-1 rounded-xl border-2 border-neutral-700 ${textColor}`}>
           <Icon icon={icon} height="2rem"/>
@@ -20,7 +20,7 @@ const Card = ({
         <h4 className="font-family-heading">{title}</h4>
         <p className="text-neutral-300">{certificationProvider}</p>
       </div >
-      <a href="#" target="_blank" rel="noopener noreferrer" className="flex gap-1 items-center text-yellow">
+      <a href="#" target="_blank" rel="noopener noreferrer" className="flex gap-1 items-center text-yellow hover:underline hover:decoration-wavy">
         <Icon icon="boxicons:medal-star-alt-2-filled"/>
         Certificado
       </a>
